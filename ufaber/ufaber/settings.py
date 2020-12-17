45e5af6bd876b0f1d2f9d4847bdbb50fb73a86e8
Sufiyan Rakhange <sufiyrakhange97@gmail.com>
2020-12-17 19:32:42 +0530

    Updated STATIC_ROOT

--- ufaber/ufaber/settings.py
+++ ufaber/ufaber/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
 
-MEDIA_URL = 'projects/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "avatar/")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
